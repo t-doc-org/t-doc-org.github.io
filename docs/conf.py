d6f8ecb8ad45007e84563649392506f1f5a8528d
Remy Blank <remy@c-space.org>
2025-06-28 15:03:24 +0200

    Serve the domain storage for all *.t-doc.org.

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: CC-BY-NC-SA-4.0
 
 from tdoc.common.defaults import *
+from tdoc.common.wsgi import hostname_re
 
 project = "t-doc.org"
 copyright = "%Y Remy Blank"
@@ -12,4 +13,11 @@ language = 'fr'
 
 html_theme_options = {
     'repository_url': 'https://github.com/t-doc-org/t-doc-org.github.io',
+}
+
+# Serve as domain storage.
+tdoc_domain_storage = {
+    **tdoc_domain_storage,
+    'allowed_origins': rf'^https://(?:{hostname_re}\.)?t-doc\.org$',
+    'allowed_keys': '^tdoc:(?:api:user|clientId)$',
 }
