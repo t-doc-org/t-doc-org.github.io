e640c42e44af4fb43d672bfbde44d5d150450536
Remy Blank <remy@c-space.org>
2025-10-04 16:51:04 +0200

    Add a logo.

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@ language = 'fr'
 
 # myst_links_external_new_tab = True
 
+html_logo = 'logo.svg'
 html_theme_options = {
     # 'repository_url': 'https://github.com/t-doc-org/t-doc-org.github.io',
 }
